51cae893d6e3742790dea71061356028d3c38d04
Indira <ibriggiler@gmail.com>
2021-07-08 23:49:59 -0300

    Server

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# searchServer
+# SERVER CHALLENGE
+
+## how to use?
+
+1. Install dependencies
+2. execute
+
+- ```bash
+    npm start
+  ```
+
+## API
+
+### all products
+
+```bash
+curl http://localhost:3000/data
+```
+
+### product by id
+
+```bash
+curl http://localhost:3000/data?_id=001
+```
+
+### product by xxxx
+
+```bash
+curl http://localhost:3000/data?key=value
+```
---
+++ server.js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("data.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+const PORT = 3001;
+
+server.use(router);
+server.listen(PORT, () =>
+  console.log(`Server is Running on localhost:${PORT}`)
+);
